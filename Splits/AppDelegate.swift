e5c60be452ed9d1dd2803dee74edd3bf72df1c84
cin-derella <shirley_cinderella@outlook.com>
2019-09-28 18:46:23 +0800

    iOS 12 compatible

--- Splits/AppDelegate.swift
+++ Splits/AppDelegate.swift
@@ -8,29 +8,52 @@
 
 import UIKit
 
-@UIApplicationMain
+class SplitDelegate : UISplitViewControllerDelegate {
+    func splitViewController(_ splitViewController: UISplitViewController, collapseSecondary secondaryViewController:UIViewController, onto primaryViewController:UIViewController) -> Bool {
+        guard let secondaryAsNavController = secondaryViewController as? UINavigationController else { return false }
+        guard let topAsDetailController = secondaryAsNavController.topViewController as? DetailViewController else { return false }
+        if topAsDetailController.detailItem == nil {
+            // Return true to indicate that we have handled the collapse by doing nothing; the secondary controller will be discarded.
+            return true
+        }
+        return false
+    }
+}
+
+
+@UIApplicationMain                                     //UISplitViewControllerDelegate
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
+    var window: UIWindow?
+    var delegate: SplitDelegate = SplitDelegate()
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        guard let window = window else { return false}
+        guard let splitViewController = window.rootViewController as? UISplitViewController else { return false}
+        guard let navigationController = splitViewController.viewControllers.last as? UINavigationController else { return false}
+        navigationController.topViewController?.navigationItem.leftBarButtonItem = splitViewController.displayModeButtonItem
+        navigationController.topViewController?.navigationItem.leftItemsSupplementBackButton = true
+        splitViewController.delegate = delegate
         return true
     }
 
     // MARK: UISceneSession Lifecycle
 
+    @available(iOS 13.0, *)
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
         // Called when a new scene session is being created.
         // Use this method to select a configuration to create the new scene with.
         return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
 
+    @available(iOS 13.0, *)
     func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
         // Called when the user discards a scene session.
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
+    
 
 
 }
---
+++ Splits/images+Names+Colors.swift
@@ -0,0 +1,26 @@
+//
+//  images+Names+Colors.swift
+//  Splits
+//
+//  Created by Dante on 2019/9/28.
+//  Copyright © 2019 cinderella. All rights reserved.
+//
+
+import UIKit
+//Images
+let bestseller = UIImage(named: "best_seller")!
+let sciencefiction  = UIImage(named: "science_fiction")!
+let smallbusiness = UIImage(named: "small_business")!
+let languagelearning = UIImage(named: "language_learning")!
+let graphicdesign = UIImage(named: "graphic_design")!
+
+let imageArray = [bestseller,sciencefiction,smallbusiness,languagelearning,graphicdesign]
+
+//Names
+let namesArray = ["Best Seller","Science Fiction","Small Business","Language Learning","Graphic Design"]
+
+//Colors
+let colorsArray = [#colorLiteral(red: 0.521568656, green: 0.1098039225, blue: 0.05098039284, alpha: 1),#colorLiteral(red: 0.9529411793, green: 0.6862745285, blue: 0.1333333403, alpha: 1),#colorLiteral(red: 0.2745098174, green: 0.4862745106, blue: 0.1411764771, alpha: 1),#colorLiteral(red: 0.9372549057, green: 0.3490196168, blue: 0.1921568662, alpha: 1),#colorLiteral(red: 0.1764705926, green: 0.4980392158, blue: 0.7568627596, alpha: 1)]
+
+
+
